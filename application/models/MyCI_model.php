6352b01c83f7d8a6abc2f066f593b330574451e6
Lou Pinton <pintonlou@gmail.com>
2021-10-05 22:22:10 -0500

    removed the dummy codes

--- application/models/MyCI_model.php
+++ application/models/MyCI_model.php
@@ -6,17 +6,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class MyCI_model extends CI_Model
 {
-    public function login()
-    {
-        $data = new stdClass();
-        $data->username = "admin";
-        $data->password = "password1";
-        return $data;
-    }
-    public function get_all_salesrep()
-    {
-        return $this->session->userdata('all_salesrep');
-    }
 }
 
 
